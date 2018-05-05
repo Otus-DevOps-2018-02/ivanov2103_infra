396c10d5397c10731ec8e73a7c20179a6f226ae5
Igor Ivanov <grazhdanin.ivanov@gmail.com>
2018-05-06 00:44:30 +0700

    Homework-10. Studied deploy and configuration mangement with ansible.

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -4,5 +4,5 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" >> /etc/apt/sources.list.d/mongodb-org-3.2.list
 apt update
 apt install -y mongodb-org
-sed -i 's/.*bindIp:.*/  bindIp: 0.0.0.0/' /etc/mongod.conf
+#sed -i 's/.*bindIp:.*/  bindIp: 0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -22,25 +22,25 @@ resource "google_compute_instance" "app" {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 
-  connection {
-    type        = "ssh"
-    user        = "appuser"
-    agent       = false
-    private_key = "${file(var.private_key_path)}"
-  }
+#  connection {
+#    type        = "ssh"
+#    user        = "appuser"
+#    agent       = false
+#    private_key = "${file(var.private_key_path)}"
+#  }
 
-  provisioner "file" {
-    source      = "../modules/app/puma.service"
-    destination = "/tmp/puma.service"
-  }
+#  provisioner "file" {
+#    source      = "../modules/app/puma.service"
+#    destination = "/tmp/puma.service"
+#  }
 
-  provisioner "remote-exec" {
-    inline = ["echo export DATABASE_URL=${var.db_ip}:27017 >> ~/.profile"]
-  }
+#  provisioner "remote-exec" {
+#    inline = ["echo export DATABASE_URL=${var.db_ip}:27017 >> ~/.profile"]
+#  }
 
-  provisioner "remote-exec" {
-    script = "../modules/app/deploy.sh"
-  }
+#  provisioner "remote-exec" {
+#    script = "../modules/app/deploy.sh"
+#  }
 }
 
 resource "google_compute_address" "app_ip" {
